e22feb902c83be4f2a8ac85ea1ae5f547f4add9f
KumarBibekAnand-2023MT93273 <2023mt93273@wilp.bits-pilani.ac.in>
2024-05-11 20:49:14 +0530

    Add book intrgration to save info to DB

--- src/app/add-book/add-book.component.ts
+++ src/app/add-book/add-book.component.ts
@@ -1,36 +1,63 @@
-// add-book.component.ts
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { BookService } from '../services/book.service';
+import { Book } from '../model/book.model';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
 
 @Component({
   selector: 'app-add-book',
   templateUrl: './add-book.component.html',
   styleUrls: ['./add-book.component.css']
 })
-export class AddBookComponent implements OnInit {
+export class AddBookComponent {
   bookForm!: FormGroup;
+  book: Book = {
+    title: '',
+    author: '',
+    genre: '',
+    bookCondition: '',
+    availability: ''
+  };
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private fb: FormBuilder, private bookService: BookService, private snackBar: MatSnackBar) {
+    this.createForm();
+  }
 
-  ngOnInit() {
-    this.bookForm = this.formBuilder.group({
+  createForm() {
+    this.bookForm = this.fb.group({
       title: ['', Validators.required],
       author: ['', Validators.required],
       genre: ['', Validators.required],
-      condition: ['', Validators.required],
+      bookCondition: ['', Validators.required],
       availability: ['', Validators.required]
     });
   }
 
   onSubmit() {
-    if (this.bookForm.valid) {
-      // Process the form data here, such as sending it to a service
-      console.log(this.bookForm.value);
-      // Reset the form after submission
-      this.bookForm.reset();
-    } else {
-      // Handle form validation errors
-      console.log("Form is invalid. Please fill in all required fields.");
-    }
+    const bookData = {
+      title: this.bookForm.get('title')!.value,
+      author: this.bookForm.get('author')!.value,
+      genre: this.bookForm.get('genre')!.value,
+      bookCondition: this.bookForm.get('bookCondition')!.value,
+      availability: this.bookForm.get('availability')!.value,
+    };
+  
+    this.bookService.saveBook(bookData).subscribe(
+      (response: Book) => {
+        console.log('Book saved successfully:', response);
+        this.snackBar.open('Book saved successfully','Ok', {
+          duration: 4000 // Duration in milliseconds
+         
+        });
+        // Optionally, you can navigate to another page or show a success message here
+      },
+      (error: any) => {
+        console.error('Error saving book:', error);
+        // Handle error - show error message or log it
+      }
+    );
   }
+  
+    
 }
